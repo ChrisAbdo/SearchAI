14bcc94ee12ac85552bc787279b644a023fbad2e
Chris Abdo <chris.j.abdo@gmail.com>
2024-06-01 08:57:28 -0400

    chore: Update .env file with NEXTAUTH_SECRET

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,6 @@ export default async function Dashboard() {
   const numSearches = session?.user?.numSearches as number;
 
   return (
-    // <div className="flex flex-col px-3 bg-muted/40 min-h-full">
-    //   <main className="mt-4">
-    //     <AskExa numSearches={numSearches} />
-    //   </main>
-    // </div>
     <div className="bg-background">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
